32fc609cf562098dd4266e88e73399d3024fb719
Kooreis <kooreis.official@gmail.com>
2023-10-19 01:46:23 -0500

    Added a private method 'backtrack' to the class. This method checks if the length of the string 'str' is equal to twice the maximum number of pairs of parentheses. If true, it pushes the string into the list and returns.

--- solution.ts
+++ solution.ts
@@ -1,7 +1,6 @@
-class ParenthesesGenerator {
-    generateParenthesis(n: number): string[] {
-        let result: string[] = [];
-        this.backtrack(result, "", 0, 0, n);
-        return result;
-    }
+private backtrack(list: string[], str: string, open: number, close: number, max: number): void {
+        if (str.length == max * 2) {
+            list.push(str);
+            return;
+        }
 }
