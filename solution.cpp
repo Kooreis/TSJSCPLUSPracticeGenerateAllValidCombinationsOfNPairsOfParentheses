438fa6fdcc36dc70d8e225ac9385ca0a82a3256c
Kooreis <kooreis.official@gmail.com>
2023-10-19 01:46:23 -0500

    In the second update, a utility function 'generateParenthesisUtil' is defined. This function is used to generate all possible combinations of well-formed parentheses. It uses recursion to generate these combinations.

--- solution.cpp
+++ solution.cpp
@@ -1,3 +1,12 @@
-#include <iostream>
-#include <vector>
-#include <string>
+void generateParenthesisUtil(int left, int right, std::string str, std::vector<std::string>& result) {
+    if (left == 0 && right == 0) {
+        result.push_back(str);
+        return;
+    }
+    if (left > 0) {
+        generateParenthesisUtil(left - 1, right, str + "(", result);
+    }
+    if (right > left) {
+        generateParenthesisUtil(left, right - 1, str + ")", result);
+    }
+}
